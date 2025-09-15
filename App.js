2f77bfd42722b103174d4e870606adf58f44e8e8
Melânia Gabrielli Silva Amaral <melaniagabriellis@gmail.com>
2025-09-15 09:47:47 -0300

    [fix] Post Data

--- App.js
+++ App.js
@@ -32,6 +32,37 @@ export default function App() {
     }
   };
 
+    const postData = async () => {
+    const API = "http://192.170.0.129:3000/api";
+    const URL = `${API}/user`;
+    setLoading(true);
+
+    //Se orientar pelo servidor quais dados obrigatórios devem ser enviados
+    body = {username: 'Graziani01', email: "graziani01@etec.sp.gov.br"};
+
+    //Padrão pelo qual será enviado o dado
+    headers = { "Content-Type": "application/json" };
+
+    page = {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers
+    }
+
+    try {
+      const response = await fetch(URL);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Projeto TCC - Turma B</Text>
@@ -42,6 +73,7 @@ export default function App() {
           <>
             <Button title="Buscar Dados" onPress={fetchData} />
             <Button title="Limpar Dados" onPress={() => setData([])} />
+              <Button title="Gravar Dados" onPress={postData} />
           </>
         )}
         {data &&
